5c1f89ded9e3a49b85b03f94631bbcb8f5a66a30
Andrew Moscardino <andrew@moscardino.net>
2024-10-17 15:25:34 -0400

    Empty project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+const App = () => {
+  return (
+    <div className="container-xl min-vh-100 vstack gap-3 py-3">
+      <h1>Hello, world!</h1>
+    </div>
+  );
+};
+
+export default App;
